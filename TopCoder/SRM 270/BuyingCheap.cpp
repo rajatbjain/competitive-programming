c82bba2521d55b54a7c15c9d16afa60e5b141d6b
Rajat Jain <rajat.rj10@gmail.com>
2014-08-17 23:30:08 +0530

    Added TopCoder

---
+++ TopCoder/SRM 270/BuyingCheap.cpp
@@ -0,0 +1,148 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <cstring>
+#include <vector>
+#include <list>
+#include <string>
+#include <map>
+#include <set>
+#include <bitset>
+#include <ctime>
+#include <deque>
+#include <sstream>
+#include <utility>
+#include <functional>
+#include <numeric>
+#include <stack>    
+#include <queue>
+#include <cmath>
+#include <cstdlib>
+#include <iomanip>
+#include <fstream>
+
+using namespace std;
+
+class BuyingCheap {
+    public:
+    int thirdBestPrice(vector<int> prices) {
+      set<int> unique;
+      for(int i = 0; i < prices.size(); ++i) {
+          unique.insert(prices[i]);
+      }
+
+      if(unique.size() < 3) return -1;
+      set<int>::iterator itr;
+      itr = unique.begin();
+      itr++; itr++;
+      return (*itr);
+    }
+};
+
+// CUT begin
+ifstream data("BuyingCheap.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(vector<int> prices, int __expected) {
+    time_t startClock = clock();
+    BuyingCheap *instance = new BuyingCheap();
+    int __result = instance->thirdBestPrice(prices);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        vector<int> prices;
+        from_stream(prices);
+        next_line();
+        int __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(prices, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1408284583;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "BuyingCheap (250 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
