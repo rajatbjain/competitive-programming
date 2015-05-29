27c131d55b2a48124fb421e17b44a2ebcbe43d03
Rajat Jain <rajat.rj10@gmail.com>
2015-05-29 13:01:31 +0530

    topcoder solutions

---
+++ TopCoder/FibonacciDiv2.cpp
@@ -0,0 +1,170 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <cstring>
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
+#include <sstream>
+#include <utility>
+#include <functional>
+#include <numeric>
+#include <stack>
+#include <queue>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <cassert>
+#include <bitset>
+#include <list>
+#include <memory.h>
+#include <typeinfo>
+#include <fstream>
+
+using namespace std;
+
+class FibonacciDiv2 {
+    public:
+    int find(int N);
+};
+
+int fib[1000010];
+
+void gen() {
+    int a = 0, b = 1;
+    fib[0] = fib[1] = 1;
+    int c = 0;
+    for( ; ; ) {
+        c = a + b;
+        if(c > 1000000) break;
+        fib[c] = 1;
+        a = b;
+        b = c;
+    }
+}
+
+int FibonacciDiv2::find(int N) {
+  gen();
+  int f = 0;
+  int ans;
+  bool found = false;
+  for (int i = N; i <= 1000000; i++) {
+    if(fib[i] == 1) {
+        found = true;
+        break;
+    }
+    f++;
+  }
+  if(found) ans = f;
+  found = false;
+  f = 0;
+  for(int i = N; i >= 1; i--) {
+    if(fib[i] == 1) {
+        found = true;
+        break;
+    }
+    f++;
+  }
+  if(found == true)
+      ans = min(f, ans);
+  return ans;
+}
+
+// @author : SensibleMadman
+// CUT begin
+ifstream data("FibonacciDiv2.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(int N, int __expected) {
+    time_t startClock = clock();
+    FibonacciDiv2 *instance = new FibonacciDiv2();
+    int __result = instance->find(N);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        int N;
+        from_stream(N);
+        next_line();
+        int __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(N, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1432882765;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "FibonacciDiv2 (250 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
